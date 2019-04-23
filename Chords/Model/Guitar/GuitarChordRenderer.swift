f3d715297e1c9e1ff43964b58b37a2ab7ddef49a
fwcd <fwcdmail@gmail.com>
2019-04-23 23:03:53 +0200

    Implement GuitarChordRenderer for SpriteKit

--- Chords/Model/Guitar/GuitarChordRenderer.swift
+++ Chords/Model/Guitar/GuitarChordRenderer.swift
@@ -28,45 +28,63 @@ struct GuitarChordRenderer: ChordRenderer {
 	}
 	
 	func render(chord: Chord) throws -> SKNode {
-//        let image = try Image(width: width, height: height)
-//        var graphics = CairoGraphics(fromImage: image)
-//        let guitarChord = try GuitarChord(from: chord, on: fretboard)
-//        let fretCount = max(minFrets, guitarChord.maxFret + 1)
-//        let stringCount = fretboard.stringCount
-//
-//        let innerWidth = Double(width) - (padding * 2)
-//        let innerHeight = (Double(height) - (padding * 2)) - gutterHeight
-//        let stringSpacing = innerWidth / Double(stringCount - 1)
-//        let fretSpacing = innerHeight / Double(fretCount - 1)
-//        let dotRadius = fretSpacing * 0.4
-//        let topLeft = Vec2(x: padding, y: padding + gutterHeight)
-//
-//        graphics.draw(Rectangle(topLeft: topLeft - Vec2(y: gutterHeight), size: Vec2(x: innerWidth, y: gutterHeight), color: fgColor, isFilled: true))
-//
-//        for stringIndex in 0..<stringCount {
-//            let position = topLeft + Vec2(x: stringSpacing * Double(stringIndex))
-//            graphics.draw(LineSegment(from: position, to: position + Vec2(y: innerHeight), color: fgColor))
-//        }
-//
-//        for fretIndex in 0..<fretCount {
-//            let position = topLeft + Vec2(y: fretSpacing * Double(fretIndex))
-//            graphics.draw(LineSegment(from: position, to: position + Vec2(x: innerWidth), color: fgColor))
-//        }
-//
-//        for dot in guitarChord.dots {
-//            let dotX = Double(dot.guitarString) * stringSpacing
-//            if dot.fret > 0 {
-//                let position = topLeft + Vec2(x: dotX, y: (Double(dot.fret - 1) + 0.5) * fretSpacing)
-//                graphics.draw(Ellipse(center: position, radius: Vec2(both: dotRadius), color: fgColor))
-//            } else {
-//                let position = topLeft + Vec2(x: dotX, y: -gutterHeight - dotRadius)
-//                graphics.draw(Ellipse(center: position, radius: Vec2(both: dotRadius), color: fgColor, isFilled: false))
-//            }
-//        }
-//
-//        return image
+        let node = SKNode()
+        let guitarChord = try GuitarChord(from: chord, on: fretboard)
+        let fretCount = max(minFrets, guitarChord.maxFret + 1)
+        let stringCount = fretboard.stringCount
+
+        let innerWidth = Double(width) - (padding * 2)
+        let innerHeight = (Double(height) - (padding * 2)) - gutterHeight
+        let stringSpacing = innerWidth / Double(stringCount - 1)
+        let fretSpacing = innerHeight / Double(fretCount - 1)
+        let dotRadius = fretSpacing * 0.4
+        let topLeft = Vec2(x: padding, y: padding + gutterHeight)
         
-        // TODO
-        return SKNode()
+        let gutterNode = SKShapeNode(rect: CGRect(origin: (topLeft - Vec2(y: gutterHeight)).asPoint, size: CGSize(width: innerWidth, height: gutterHeight)))
+        gutterNode.fillColor = fgColor
+        node.addChild(gutterNode)
+        
+        for stringIndex in 0..<stringCount {
+            let position = topLeft + Vec2(x: stringSpacing * Double(stringIndex))
+            let stringNode = SKShapeNode()
+            let path = CGMutablePath()
+            path.move(to: position.asPoint)
+            path.addLine(to: (position + Vec2(y: innerHeight)).asPoint)
+            stringNode.strokeColor = fgColor
+            stringNode.path = path
+            node.addChild(stringNode)
+        }
+
+        for fretIndex in 0..<fretCount {
+            let position = topLeft + Vec2(y: fretSpacing * Double(fretIndex))
+            let fretNode = SKShapeNode()
+            let path = CGMutablePath()
+            path.move(to: position.asPoint)
+            path.addLine(to: (position + Vec2(x: innerWidth)).asPoint)
+            fretNode.strokeColor = fgColor
+            fretNode.path = path
+            node.addChild(fretNode)
+        }
+
+        for dot in guitarChord.dots {
+            let dotX = Double(dot.guitarString) * stringSpacing
+            let dotNode: SKShapeNode
+            let radius = Vec2(both: dotRadius)
+            
+            if dot.fret > 0 {
+                let position = topLeft + Vec2(x: dotX, y: (Double(dot.fret - 1) + 0.5) * fretSpacing)
+                dotNode = SKShapeNode(ellipseIn: CGRect(origin: (position - radius).asPoint, size: radius.asSize))
+                dotNode.fillColor = fgColor
+            } else {
+                let position = topLeft + Vec2(x: dotX, y: -gutterHeight - dotRadius)
+                dotNode = SKShapeNode(ellipseIn: CGRect(origin: (position - radius).asPoint, size: radius.asSize))
+                dotNode.strokeColor = fgColor
+                dotNode.fillColor = .clear
+            }
+            
+            node.addChild(dotNode)
+        }
+
+        return node
 	}
 }
---
+++ Chords/Utils/Vec2.swift
@@ -0,0 +1,72 @@
+import CoreGraphics
+
+public typealias VecComponent = SignedNumeric
+
+public struct Vec2<T: VecComponent>: CustomStringConvertible {
+	public let x: T
+	public let y: T
+	public var asTuple: (x: T, y: T) { return (x: x, y: y) }
+	public var xInverted: Vec2<T> { return Vec2(x: -x, y: y) }
+	public var yInverted: Vec2<T> { return Vec2(x: x, y: -y) }
+	public var description: String { return "(\(x), \(y))" }
+	
+	public init(x: T = 0, y: T = 0) {
+		self.x = x
+		self.y = y
+	}
+	
+	public init(both value: T) {
+		x = value
+		y = value
+	}
+	
+	public static func +(lhs: Vec2<T>, rhs: Vec2<T>) -> Vec2<T> {
+		return Vec2(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+	}
+	
+	public static func -(lhs: Vec2<T>, rhs: Vec2<T>) -> Vec2<T> {
+		return Vec2(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+	}
+	
+	public static func *(lhs: Vec2<T>, rhs: Vec2<T>) -> Vec2<T> {
+		return Vec2(x: lhs.x * rhs.x, y: lhs.y * rhs.y)
+	}
+	
+	public static func *(lhs: Vec2<T>, rhs: T) -> Vec2<T> {
+		return Vec2(x: lhs.x * rhs, y: lhs.y * rhs)
+	}
+	
+	public prefix static func -(operand: Vec2<T>) -> Vec2<T> {
+		return Vec2(x: -operand.x, y: -operand.y)
+	}
+	
+	public func dot(other: Vec2<T>) -> T {
+		return (x * other.x) + (y * other.y)
+	}
+	
+	public func map<R: VecComponent>(mapper: (T) -> R) -> Vec2<R> {
+		return Vec2<R>(x: mapper(x), y: mapper(y))
+	}
+	
+	public func with(x newX: T) -> Vec2<T> {
+		return Vec2(x: newX, y: y)
+	}
+	
+	public func with(y newY: T) -> Vec2<T> {
+		return Vec2(x: x, y: newY)
+	}
+}
+
+extension Vec2 where T: BinaryFloatingPoint {
+	public var length: T { return ((x * x) + (y * y)).squareRoot() }
+	public var floored: Vec2<Int> { return Vec2<Int>(x: Int(x.rounded(.down)), y: Int(y.rounded(.down))) }
+}
+
+extension Vec2 where T == Int {
+	public var asDouble: Vec2<Double> { return map { Double($0) } }
+}
+
+extension Vec2 where T == Double {
+    public var asPoint: CGPoint { return CGPoint(x: x, y: y) }
+    public var asSize: CGSize { return CGSize(width: x, height: y) }
+}
