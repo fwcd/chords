fff00a99a92685bfeb437223a5323d500a4de3f6
fwcd <fwcdmail@gmail.com>
2019-04-23 23:20:58 +0200

    Invert y-coordinates in the chord renderer to match SpriteKit's coordinate system

--- Chords/Model/Guitar/GuitarChordRenderer.swift
+++ Chords/Model/Guitar/GuitarChordRenderer.swift
@@ -10,8 +10,8 @@ struct GuitarChordRenderer: ChordRenderer {
 	private let minFrets: Int
 	
 	init(
-		width: Int = 150,
-		height: Int = 200,
+		width: Int = 300,
+		height: Int = 400,
 		gutterHeight: Double = 10,
 		padding: Double = 20,
 		fgColor: SKColor = .white,
@@ -38,7 +38,7 @@ struct GuitarChordRenderer: ChordRenderer {
         let stringSpacing = innerWidth / Double(stringCount - 1)
         let fretSpacing = innerHeight / Double(fretCount - 1)
         let dotRadius = fretSpacing * 0.4
-        let topLeft = Vec2(x: padding, y: padding + gutterHeight)
+        let topLeft = Vec2(x: -innerWidth / 2, y: innerHeight / 2)
         
         let gutterNode = SKShapeNode(rect: CGRect(origin: (topLeft - Vec2(y: gutterHeight)).asPoint, size: CGSize(width: innerWidth, height: gutterHeight)))
         gutterNode.fillColor = fgColor
@@ -49,14 +49,14 @@ struct GuitarChordRenderer: ChordRenderer {
             let stringNode = SKShapeNode()
             let path = CGMutablePath()
             path.move(to: position.asPoint)
-            path.addLine(to: (position + Vec2(y: innerHeight)).asPoint)
+            path.addLine(to: (position - Vec2(y: innerHeight)).asPoint)
             stringNode.strokeColor = fgColor
             stringNode.path = path
             node.addChild(stringNode)
         }
 
         for fretIndex in 0..<fretCount {
-            let position = topLeft + Vec2(y: fretSpacing * Double(fretIndex))
+            let position = topLeft - Vec2(y: fretSpacing * Double(fretIndex))
             let fretNode = SKShapeNode()
             let path = CGMutablePath()
             path.move(to: position.asPoint)
@@ -70,14 +70,15 @@ struct GuitarChordRenderer: ChordRenderer {
             let dotX = Double(dot.guitarString) * stringSpacing
             let dotNode: SKShapeNode
             let radius = Vec2(both: dotRadius)
+            let diameter = radius * 2
             
             if dot.fret > 0 {
-                let position = topLeft + Vec2(x: dotX, y: (Double(dot.fret - 1) + 0.5) * fretSpacing)
-                dotNode = SKShapeNode(ellipseIn: CGRect(origin: (position - radius).asPoint, size: radius.asSize))
+                let position = topLeft + Vec2(x: dotX, y: -(Double(dot.fret - 1) + 0.5) * fretSpacing)
+                dotNode = SKShapeNode(ellipseIn: CGRect(origin: (position - radius).asPoint, size: diameter.asSize))
                 dotNode.fillColor = fgColor
             } else {
-                let position = topLeft + Vec2(x: dotX, y: -gutterHeight - dotRadius)
-                dotNode = SKShapeNode(ellipseIn: CGRect(origin: (position - radius).asPoint, size: radius.asSize))
+                let position = topLeft + Vec2(x: dotX, y: gutterHeight + dotRadius)
+                dotNode = SKShapeNode(ellipseIn: CGRect(origin: (position - radius).asPoint, size: diameter.asSize))
                 dotNode.strokeColor = fgColor
                 dotNode.fillColor = .clear
             }
