0457864fc915ce6114b21b2552ca8c1bd9e6517c
fwcd <fwcdmail@gmail.com>
2019-04-23 23:13:00 +0200

    Integrate basic chord rendering

--- Chords/GameScene.swift
+++ Chords/GameScene.swift
@@ -12,11 +12,11 @@ import Foundation
 
 class GameScene: SKScene {
     private var label: SKLabelNode?
-    private var spinnyNode: SKShapeNode?
     private var lastPress: Date?
+    private var chordNode: SKNode?
+    private let chordRenderer: ChordRenderer = GuitarChordRenderer()
     
     override func didMove(to view: SKView) {
-        
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//centerLabel") as? SKLabelNode
         if let label = self.label {
@@ -24,54 +24,28 @@ class GameScene: SKScene {
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//        if let spinnyNode = self.spinnyNode {
+//            spinnyNode.lineWidth = 2.5
+//
+//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                              SKAction.fadeOut(withDuration: 0.5),
+//                                              SKAction.removeFromParent()]))
+//        }
+    }
+    
+    private func update(chord rawChord: String) {
+        label?.text = rawChord
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        if let chord = try? Chord(of: rawChord) {
+            if let previousChordNode = chordNode {
+                removeChildren(in: [previousChordNode])
+            }
+            chordNode = try? chordRenderer.render(chord: chord)
+            if let newChordNode = chordNode {
+                addChild(newChordNode)
+            }
         }
-    }
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
     }
     
     override func keyDown(with event: NSEvent) {
@@ -79,9 +53,9 @@ class GameScene: SKScene {
         
         if let chars = event.characters {
             if secondsSinceLastPress > 1 {
-                label?.text = chars
+                update(chord: chars)
             } else if let oldText = label?.text {
-                label?.text = oldText + chars
+                update(chord: oldText + chars)
             }
         }
         
